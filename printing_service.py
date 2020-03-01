007221433b46c7cf386202ab70c14b207d65f4b8
bertrandlo@pds.com <bertrandlo@pds.com>
2020-03-01 19:17:45 +0800

    [fix]  將本機loop 改成設預設

--- printing_service.py
+++ printing_service.py
@@ -85,5 +85,5 @@ if __name__ == '__main__':
     # app.run()  # flask dev server
     settings = INIReader()
     serve(app,
-          listen=settings.get("network", "listen"),
+          listen=("127.0.0.1:5000  " + settings.get("network", "listen")),
           url_scheme='http')  # waitress production serve
